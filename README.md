5040e28a820c8ba13c1851871ecd2592013598ce
EGARRISXN <egarrisxn@gmail.com>
2024-04-19 15:20:28 -0400

    redoing projects from bootcamp.

---
+++ README.md
@@ -0,0 +1,53 @@
+# Weather Dashboard
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+_Initially created in April of 2023 as a mini-project for my UCF Full-Stack Web Development Bootcamp._
+
+## Description
+
+This weather application shows you current weather, along with a 5 day forecast. The searches is cached for easy viewing access if you need to revisit. Stay cool out there!
+
+## Technologies
+
+- HTML5: Markup language for structuring the web page.
+- CSS3 (Bootstrap): Used for styling and layout of the application.
+- JavaScript (jQuery): Provides interactivity and dynamic behavior.
+- Day.js: Library for parsing, validating, manipulating, and formatting dates and times.
+- Local Storage API: Allows data to be stored locally within the user's browser.
+
+## Installation
+
+To install this project for local development:
+
+1. Clone the repository to your local machine.
+2. Open the project directory.
+3. Open the `index.html` file in a web browser to view the application.
+
+## Usage
+
+To use the application:
+
+1. Open the `index.html` file in a web browser.
+2. TBD
+3. TBD
+
+## To-Do
+
+- **Create button to clear search history.**
+- **Align icons next to all the dates.**
+- **Play around with the css and structure of application.**
+- **Update labels to make accessible.**
+- **Update the usage section of the readme.**
+
+## Acknowledgements
+
+My classmates all had a hand in assisting each other to victory for this project.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Questions
+
+Feel free to reach out to me here on [GitHub](https://github.com/EGARRISXN), or directly via email at egarrisxn@gmail.com if you have any questions.
---
+++ assets/js/script.js
@@ -0,0 +1,118 @@
+$(document).ready(function () {
+  let localStorageData = new Set(
+    JSON.parse(localStorage.getItem("city")) || []
+  );
+  const apiKey = "e081906e41053d0045aef1f5836faf73";
+  const historyContainer = $("#search-history");
+  const forecastContainer = $("#forecastDays");
+
+  function showHistory() {
+    historyContainer.empty();
+    localStorageData.forEach((data) => {
+      const button = $("<button>")
+        .addClass(
+          "btn-history btn text-black btn-secondary font-weight-bold mb-2 mt-2"
+        )
+        .text(data)
+        .attr("data-city", data)
+        .on("click", function (event) {
+          event.preventDefault();
+          getApiData(data);
+        });
+      historyContainer.append(button);
+    });
+  }
+
+  function getApiData(city) {
+    const forecastData = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=6&appid=${apiKey}&units=imperial`;
+    fetch(forecastData)
+      .then((response) => response.json())
+      .then((data) => {
+        localStorageData.add(data.city.name);
+        localStorage.setItem(
+          "city",
+          JSON.stringify(Array.from(localStorageData))
+        );
+        loadMainWeather(data);
+        loadForecastWeather(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("An error occurred while fetching weather data.");
+      });
+  }
+
+  function loadMainWeather(data) {
+    const now = dayjs().format(" (M/DD/YYYY)");
+    const mainName = $("<div>")
+      .attr("id", "main-name")
+      .text(data.city.name + now);
+    const mainIcon = $("<img>")
+      .attr("id", "main-icon")
+      .attr("src", getWeatherIcon(data.list[0].weather[0].icon));
+    const mainTemp = $("<div>")
+      .attr("id", "main-temp")
+      .text("Temp: " + data.list[0].temp.day + " F");
+    const mainWind = $("<div>")
+      .attr("id", "main-wind")
+      .text("Wind: " + data.list[0].speed + " mph");
+    const mainHumidity = $("<div>")
+      .attr("id", "main-humidity")
+      .text("Humidity: " + data.list[0].humidity + " %");
+    $("#mainWeather").html([
+      mainName,
+      mainIcon,
+      mainTemp,
+      mainWind,
+      mainHumidity,
+    ]);
+  }
+
+  function loadForecastWeather(data) {
+    forecastContainer.empty();
+    for (let i = 1; i <= 5; i++) {
+      const card = $("<div>")
+        .addClass("card rounded-0 me-4 bg-dark text-light mb-3 pl-3")
+        .css("width", "10.3rem");
+      const cardBody = $("<div>").addClass("card-body").attr("id", `card${i}`);
+      const day = $("<div>").addClass("day").text(formatDate(data.list[i].dt));
+      const icon = $("<img>")
+        .addClass("icon")
+        .attr("src", getWeatherIcon(data.list[i].weather[0].icon));
+      const temp = $("<div>")
+        .addClass("temp")
+        .attr("id", `temp${i}`)
+        .text(`Temp: ${data.list[i].temp.day} F`);
+      const wind = $("<div>")
+        .addClass("wind")
+        .attr("id", `wind${i}`)
+        .text(`Wind: ${data.list[i].speed} mph`);
+      const humidity = $("<div>")
+        .addClass("humidity")
+        .attr("id", `humidity${i}`)
+        .text(`Humidity: ${data.list[i].humidity} %`);
+      cardBody.append(day, icon, temp, wind, humidity);
+      card.append(cardBody);
+      forecastContainer.append(card);
+    }
+  }
+
+  function formatDate(dt) {
+    return dayjs.unix(dt).format("M/DD/YYYY");
+  }
+
+  function getWeatherIcon(iconCode) {
+    return `http://openweathermap.org/img/wn/${iconCode}.png`;
+  }
+
+  $(".btn").on("click", function (event) {
+    event.preventDefault();
+    const searchCity = $("#city").val();
+    $("#city").val("");
+    getApiData(searchCity);
+    showHistory();
+  });
+
+  // Initial call to display search history
+  showHistory();
+});
